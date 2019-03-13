5897b26272dc732cc2d332b66ed3428b047571e0
Christa Stephens <ChristaStephens@outlook.com>
2019-03-13 17:51:23 -0400

    oh god, after what felt like eons I realized that I needed a private method. Stared at this thing for quite a bit.

--- src/working/DaysWorked.java
+++ src/working/DaysWorked.java
@@ -6,28 +6,30 @@ import org.junit.Test;
 
 public class DaysWorked {
 
-/*	Story:
-		As a babysitter
-		In order to get paid for 1 night of work
-		I want to calculate my nightly charge*/
-	
-	//Project Goal: Create test to show Mellie being paid
-	
-	//Start with calc time for 1 hour of work
+	/*
+	 * Story: As a babysitter In order to get paid for 1 night of work I want to
+	 * calculate my nightly charge
+	 */
+
+	// Project Goal: Create test to show Mellie being paid
+
+	// Start with calc time for 1 hour of work
 	@Test
 	public void calculatepayforworkafterstarttimeat12hourly() {
-		//5 is hours worked if start at 5 pm til 10p
-		MellieWageCalculator potentialPay = new MellieWageCalculator(5); 
-		//assert equals gives -> (expected, actual)
+		// 5 is hours worked if start at 5 pm til 10p
+		//the 5 & the 2 is passed into hours worked private class
+		//so that it won't be changed later.
+		MellieWageCalculator potentialPay = new MellieWageCalculator(5);
+		// assert equals gives -> (expected, actual)
 		assertEquals(60, potentialPay.calculatePay());
 	}
-	
+
 	@Test
 	public void calculatepayforworkafter10pmat8hourly() {
-		//2 hours worked if start at 10pm til 12 pm
+		// 2 hours worked if start at 10pm til 12 pm
 		MellieWageCalculator potentialPay = new MellieWageCalculator(2);
 		assertEquals(16, potentialPay.calculatePay());
-		
+
 	}
 
 }
--- src/working/MellieWageCalculator.java
+++ src/working/MellieWageCalculator.java
@@ -1,27 +1,30 @@
 package working;
 
 public class MellieWageCalculator {
-
-	public MellieWageCalculator(int potentialPay) {
-		// TODO Auto-generated constructor stub
+	
+	private int hoursWorked= 0;
+	public MellieWageCalculator(int i) {
+	    // TODO Auto-generated constructor stub
+	    this.hoursWorked= i;
 	}
 
 	public Object calculatePay() {
 		int potentialPayBefore10 = 12;
 		int potentialPayAfter10 = 8;
-		
-		//$12 hour * 5 hours worked
-		potentialPayBefore10 = 12*5;
-		potentialPayAfter10 = 8*2;
-		
-		int totalPayAfter10 = potentialPayAfter10;
+
+		// $12 hour * 5 hours worked
+		//$8 hour * 2 hours worked
+		//hoursWorked is being passed from MellieWageCalc
+		 potentialPayBefore10 = 12 * hoursWorked;
+		 potentialPayAfter10 = 8 * hoursWorked;
+
 		// TODO Auto-generated method stub
-		
-		if (potentialPayBefore10 != potentialPayAfter10) {
-			return totalPayAfter10;
-		}
-		
-		return potentialPayBefore10;
+
+		if (potentialPayBefore10 < 60) {
+			return potentialPayAfter10;
+		} else
+			return potentialPayBefore10;
+
 	}
 
 }
