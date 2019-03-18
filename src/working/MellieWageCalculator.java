127b5837b3a9f004a7534df67b8ab3cb4c46674a
Christa Stephens <ChristaStephens@outlook.com>
2019-03-18 11:53:05 -0400

    Quite a few changes: names of vars simplified, additionally logical to solve for hours instead of pay. Test 3 still fails.

--- src/working/MellieWageCalculator.java
+++ src/working/MellieWageCalculator.java
@@ -10,23 +10,53 @@ public class MellieWageCalculator {
 	}
 
 	public Object calculatePay() {
-		int potentialPayBefore10 = 12;
-		int potentialPayAfter10 = 8;
+		int payBefore10 = 12;
+		int payAfter10 = 8;
+		int payAfter12 = 16;
 
 		// $12 hour * 5 hours worked
 		// $8 hour * 2 hours worked
 		// hoursWorked is being passed from MellieWageCalc
-		potentialPayBefore10 = 12 * hoursWorked;
-		potentialPayAfter10 = 8 * hoursWorked;
+		payBefore10 = 12 * hoursWorked;
+		payAfter10 = 8 * hoursWorked;
+		payAfter12 = 16 * hoursWorked;
 
-		// Mental note:if potential money before 10 is less than 60
-		// give back potential money after 10
-
-		if (potentialPayBefore10 < 60) {
-			return potentialPayAfter10;
+		//Mental note:if potential money before 10 is less than 60
+		//give back potential money after 10
+		//changed to hoursWorked in conditional so that user
+		//can input the hours that they worked to generate pay.
+		
+		if (hoursWorked != 5) {
+			return payAfter10;
+		}
+		if (hoursWorked < 5) {
+			return payAfter12;
 		} else
-			return potentialPayBefore10;
+			return payBefore10;
+		
 
-	}
+//		else if(hoursWorked>5) {
+//			return payAfter10;
+//		}
+//		else return payBefore10 ;
+//		
+//		}
+
+//		if (potentialPayBefore10 < 60) {
+//			return potentialPayAfter12;
+//		} 
+//		else if (potentialPayBefore10<100) {
+//			return potentialPayBefore10;
+//		}
+//		else
+//			return potentialPayAfter10;
+//
+//	}
 
 }
+	}
+
+//something like this:
+//else if(potentialPayBefore10 != potentialPayAfter12) {
+//	return potentialPayAfter12;
+//}
